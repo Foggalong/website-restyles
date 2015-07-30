975f7f56a002cd868452aaa14f801256a63b91a5
Joshua Fogg <joshua.h.fogg@gmail.com>
2015-07-30 17:33:23 +0100

    Started Reddit script

---
+++ styles/reddit.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name          Minimal Reddit
+// @namespace     http://userstyles.org
+// @description	  Makes Reddit a bit cleaner
+// @author        Foggalong
+// @homepage      http://fogg.me.uk
+// @include       *.reddit.com/*
+// @run-at        document-start
+// @grant         none
+// ==/UserScript==
+
+/* Allows for CSS editing */
+function addCSS(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+/* Removed beta symbol */
+addCSS('.a.beta-link { display:none !important; }')
+
+/* Removes the footer */
+addCSS('.div.footer-parent { display:none !important; }')
+addCSS('.p.debuginfo { display:none !important; }')
