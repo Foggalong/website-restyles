f12484ca29a97294c51fd71535a419db9e3a735e
Joshua Fogg <joshua.h.fogg@gmail.com>
2015-07-31 22:06:00 +0100

    Started Google+

---
+++ styles/plus.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name          Minimal Google+
+// @namespace     http://userstyles.org
+// @description	  Makes Google+ a bit cleaner
+// @author        Foggalong
+// @homepage      http://fogg.me.uk
+// @include       https://plus.google.com/*
+// @run-at        document-start
+// @grant         none
+// ==/UserScript==
+
+/* Allows for CSS editing */
+function addCSS(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+/* Removes annoying posts */
+addCSS('.nja { display:none !important; }')
