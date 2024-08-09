ffa5caeeac31d3631b9271e132434b3b7198918a
Joshua Fogg <joshua.h.fogg@gmail.com>
2024-08-09 16:31:22 +0100

    Updated template to new format

--- styles/template.user.js
+++ styles/template.user.js
@@ -3,22 +3,20 @@
 // @namespace     http://userstyles.org
 // @description	  Makes $SITENAME a bit cleaner
 // @author        Foggalong
-// @homepage      http://fogg.me.uk
+// @homepage      http://fogg.uk
 // @include       https://www.$WEBSITE-URL.com/*
 // @run-at        document-start
 // @grant         none
 // ==/UserScript==
 
-/* Allows for CSS editing */
-function addCSS(css) {
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
-    style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = css;
-    head.appendChild(style);
-}
 
-/* Removes part of interface */
-addCSS('.html.element { display:none !important; }');
+// styles to inject as a multiline JavaScript string
+var styles = `
+  .html.element { display:none !important; }
+`
+
+
+// code for injecting `styles` into the stylesheet
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
